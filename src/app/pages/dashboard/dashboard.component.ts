93658af49a815898a55d1849ab70257ecfcd5c34
AntoineVigneronIsisCastres <antoine.vigneron@etud.univ-jfc.fr>
2023-09-12 17:52:59 +0200

    Added routing in dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -25,17 +25,30 @@ export class DashboardComponent {
   }
 
   loadPractitioner() {
-    return this.webService.getPractitioner().subscribe((data) => {
+    return this.webService.getPractitioner().subscribe((data : Practitioner) => {
       this.practitioner = data;
       console.log(this.practitioner)
     })
   }
 
   loadPatients() {
-    return this.webService.getPatients().subscribe((data) => {
+    return this.webService.getPatients().subscribe((data : Patient[]) => {
       this.patients = data;
-      console.log(this.patients)
+      console.log(data[0])
     })
+  }
+
+  calculAge(birthDate : string) {
+    const today = new Date();
+    const birthDateObj = new Date(birthDate);
+    let age = today.getFullYear() - birthDateObj.getFullYear();
+    const monthDiff = today.getMonth() - birthDateObj.getMonth();
+  
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDateObj.getDate())) {
+      age--;
+    }
+  
+    return age;
   }
 
   goToStatistiques(){
